09e25b827336e32f3234942977c3870dde37406a
AruzhanBazarbai <aruzhanart2003@mail.ru>
2021-02-09 19:31:07 +0600

    added1111

--- tsis3/5.py
+++ tsis3/5.py
@@ -1,15 +1,16 @@
+#Задача №3853. Большой сдвиг
 a=list(map(int,input().split()))
 x=int(input())
 
-if x>0:
-    for i in range(x-1,len(a)+x-1):
-        z=(abs(len(a)-i))%len(a)
+if x>=0:
+    for i in range(len(a)):
+        z=(len(a)-x+i)%len(a)
         print(a[z],end=' ')
     
 elif x<0:
-    x*=-1
-    for i in range(x,len(a)+x):
-        z=(len(a)+i)%len(a)
+    x=len(a)+x
+    for i in range(len(a)):
+        z=(len(a)-x+i)%len(a)
         print(a[z],end=' ')
 
 
